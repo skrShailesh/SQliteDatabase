a654787b9f6c99aee3036d0704127ba2700982f1
Shailesh kumar <skrshailesh1996@gmail.com>
2023-06-05 12:53:28 +0530

    Update MyDatabaseHelper.java

--- app/src/main/java/com/example/sqlitedatabase/MyDatabaseHelper.java
+++ app/src/main/java/com/example/sqlitedatabase/MyDatabaseHelper.java
@@ -9,35 +9,36 @@ import androidx.annotation.Nullable;
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
     private Context context;
-    public static final String DATABASE_NAME = "Book Library.db";
-    public static final int DATABASE_VESION = 1;
+    private static final String DATABASE_NAME = "Book Library.db";
+    private static final int DATABASE_VERSION = 1;
 
-    public static final String TABLE_NAME = "my library";
-    public static final String COLUMN_ID = "id";
-    public static final String COLUMN_TITLE = "book title";
-    public static final String COLUMN_AUTHOR = "book author";
-    public static final String COLUMN_PAGES = "book pages";
+    private static final String TABLE_NAME = "my library";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_TITLE = "book title";
+    private static final String COLUMN_AUTHOR = "book author";
+    private static final String COLUMN_PAGES = "book pages";
 
 
     public MyDatabaseHelper(@Nullable Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VESION);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query =
-                "CREATE TABLE " + TABLE_NAME +
-                        " (" + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENTATION, " +
-                        COLUMN_TITLE + "TEXT, " +
-                        COLUMN_AUTHOR + "TEXT, " +
-                        COLUMN_PAGES + "INTEGER);";
+        String query = "CREATE TABLE " + TABLE_NAME + " (" +
+                        COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENTATION, " +
+                        COLUMN_TITLE + " TEXT, " +
+                        COLUMN_AUTHOR + " TEXT, " +
+                        COLUMN_PAGES + " INTEGER);";
+        db.execSQL(query);
 
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
     }
 }
